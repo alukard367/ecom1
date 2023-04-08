c02c99ad94a103b3aa856c1ffadc5c45bf9acddb
alukard367 <motivatedCoder367@gmail.com>
2023-04-08 06:31:25 -0700

    vids ends in 57mins

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,21 @@
-console.log('hello test')
+//Copy Menu For Mobile
+
+function copyMenu() {
+    //Copy Inside .dpt-cat to .departments
+
+    let dptCategory = document.querySelector(".dpt-cat")
+    let dptPlace = document.querySelector(".departments")
+    dptPlace.innerHTML = dptCategory.innerHTML
+
+    // copy inside nav to nav
+    let mainNav = document.querySelector(".header-nav nav")
+    let navPlace = document.querySelector(".off-canvas nav")
+    navPlace.innerHTML = mainNav.innerHTML
+
+    //copy .header-top .wrapper to .theTop-nav
+    let topNav = document.querySelector(".header-top wrapper")
+    let topPlace = document.querySelector(".off-canvas .theTop-nav")
+    topPlace.innerHTML = topNav.innerHTML
+}
+
+copyMenu()
